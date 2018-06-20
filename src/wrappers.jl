36c93c3b4184575eecfdcbc55208ba22d85191e7
Ben Murrell <murrellb@gmail.com>
2018-06-20 09:23:41 +0000

    Added stuff to help with RIFRAF runs.

--- src/wrappers.jl
+++ src/wrappers.jl
@@ -62,6 +62,58 @@ function usearch_ref_database(inFastXPath, outFastaPath, referencePath;
     return names2nd, seqs2nd
 end
 
+function substring_coords(query,ref)
+    start_coord = searchindex(ref,query)
+    if start_coord == 0
+        return false        
+    end
+    return (start_coord,start_coord+length(query)-1)
+end
+
+function trim_fastq_entry(trimmed_seq,fastq_read,phred_arr)
+    trimmed_seq = uppercase(trimmed_seq)
+    fastq_read = uppercase(fastq_read)
+    #first look for seq in current orientation
+    revcomp = false
+    try_coords = substring_coords(trimmed_seq,fastq_read)
+    #then try the reverse complement
+    if try_coords==false
+        revcomp = true
+        fastq_read = reverse_complement(fastq_read)
+        try_coords = substring_coords(trimmed_seq,fastq_read)
+        if try_coords==false
+            return false
+        else
+            phred_arr = reverse(phred_arr)
+        end
+    end
+    return fastq_read[try_coords[1]:try_coords[2]],phred_arr[try_coords[1]:try_coords[2]]
+end
+
+function usearch_trim_fastq_with_phreds(inFastqPath, referencePath; divergence=0.9, outFastqPath="")
+    #READ IN THE FASTQ FILE
+    fastqseqs, fastqphreds, fastqnames = NextGenSeqUtils.read_fastq(inFastqPath)
+    fastq_name_dic = array2index_dict(fastqnames)
+    
+    #USEARCH TO TRIM, BUT ONLY STORES .FASTA OUTPUT
+    tempOutPath = "/tmp/usearch9tempOutFile"
+    run(`$(PATHS.usearch) -usearch_global $inFastqPath -db $referencePath -id $divergence -fastapairs $tempOutPath -strand both -top_hit_only`)
+    names, seqs = read_fasta_with_names(tempOutPath)
+    ref_names = [names[i*2] for i in 1:Int64(round(length(names)/2))]
+    
+    names2nd = [names[i*2-1] for i in 1:Int64(round(length(names)/2))]
+    seqs2nd = [degap(seqs[i*2-1]) for i in 1:Int64(round(length(names)/2))]
+    
+    #trim_fastq_entry returns read,phred
+    trimmed_fastq = [(fastqnames[fastq_name_dic[names2nd[i]]],trim_fastq_entry(seqs2nd[i],fastqseqs[fastq_name_dic[names2nd[i]]],fastqphreds[fastq_name_dic[names2nd[i]]])) for i in 1:length(names2nd)]
+
+    if outFastqPath != ""
+        write_fastq(outFastqPath, [i[2][1] for i in trimmed_fastq], [i[2][2] for i in trimmed_fastq],names=[i[1] for i in trimmed_fastq])
+    end
+    
+    return ref_names,trimmed_fastq
+end
+
 #-------MAFFT WRAPPER--------
 """
     mafft(inpath, outpath; path="", flags::Vector{String}=String[], kwargs...)
