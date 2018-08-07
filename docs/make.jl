bcd3dab86a150032422c4aecf6aab71409f1e82b
Nick Bavafa <nbavafa@gmail.com>
2018-08-07 11:20:31 -0700

    Added markdowns

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,7 @@
 using Documenter, NextGenSeqUtils
-
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo = "github.com/MurrellGroup/NextGenSeqUtils.jl.git",
+    julia = "nightly",
+    osx = "osx"
+ 
 makedocs()
