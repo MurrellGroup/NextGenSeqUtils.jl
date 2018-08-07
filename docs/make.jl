fdf03a6fa43baf30257d70c58d0c345ad0d25065
Nick Bavafa <nbavafa@gmail.com>
2018-08-07 11:04:58 -0700

    gh-pages branch

--- docs/make.jl
+++ docs/make.jl
@@ -1,31 +1,3 @@
-# Run `julia make.jl` in this folder to generate .html pages in a build/
-# directory, then open index.md for docs
-
-
 using Documenter, NextGenSeqUtils
 
-makedocs(
-    format = :html,
-    sitename = "NextGenSeqUtils.jl",
-    modules = [NextGenSeqUtils],
-    pages = [
-        "index.md",
-        "Documentation" => [
-            "align.md",
-            "hmm.md",
-            "io.md",
-            "kmers.md",
-            "orient.md",
-            "paths.md",
-            "phreds.md",
-            "simulation.md",
-            "utils.md"
-	   ]
-    ]
-)
-
-deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/MurrellGroup/NextGenSeqUtils.jl.git",
-    julia = "0.6",
-)
-
+makedocs()
