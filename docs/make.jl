6dde27a2db3d2a3a22194487da9a1615a9176625
Nick Bavafa <nbavafa@gmail.com>
2018-08-07 10:48:16 -0800

    Deployed f2a6c55 with MkDocs version: 0.17.5

--- docs/make.jl
+++
@@ -1,7 +0,0 @@
-using Documenter, NextGenSeqUtils
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/MurrellGroup/NextGenSeqUtils.jl.git",
-    julia = "nightly",
-    osx = "osx"
- 
-makedocs()
