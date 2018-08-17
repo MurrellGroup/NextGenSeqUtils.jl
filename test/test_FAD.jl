c2e6369f14d3382d9ca9a1be7c9acf27b4853d65
Nicholas Bavafa <nbavafa@gmail.com>
2018-08-17 11:42:59 -0700

    Added tests

--- test/test_FAD.jl
+++ test/test_FAD.jl
@@ -2,7 +2,7 @@
     
     @testset "FAD" begin
         out1, out2 = FAD(vcat([reverse(nl43env) for i in 1:10], [nl43env for i in 1:10]))
-        @test sort(out1) == sort([nl43env, reverse(nl43env)] && out2 == [10.0, 10.0])
+        @test (sort(out1) == sort([nl43env, reverse(nl43env)]) && out2 == [10.0, 10.0])
     end
 
 end
